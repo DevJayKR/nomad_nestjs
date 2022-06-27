624c5cc30117cd08fa17bb04d1ff838ffedf85bc
최준성 <jay@jay.local>
2022-06-27 23:00:59 +0900

    learning nestjs

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,43 @@
+import { MoviesService } from './movies.service';
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Patch,
+    Post,
+} from '@nestjs/common';
+import { Movie } from './entities/movie.entity';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
+
+@Controller('movies')
+export class MoviesController {
+    constructor(private readonly moviesService: MoviesService) {}
+
+    @Get('/')
+    getAll(): Movie[] {
+        return this.moviesService.getAll();
+    }
+
+    @Get('/:id')
+    getOne(@Param('id') id: number): Movie {
+        return this.moviesService.getOne(id);
+    }
+
+    @Post()
+    create(@Body() data: CreateMovieDto) {
+        return this.moviesService.create(data);
+    }
+
+    @Delete('/:id')
+    delete(@Param('id') id: number) {
+        return this.moviesService.deleteOne(id);
+    }
+
+    @Patch('/:id')
+    patch(@Param('id') id: number, @Body() updateData: UpdateMovieDto) {
+        return this.moviesService.update(id, updateData);
+    }
+}
---
+++ src/movies/movies.service.ts
@@ -0,0 +1,46 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
+import { Movie } from './entities/movie.entity';
+
+@Injectable()
+export class MoviesService {
+    private movies: Movie[] = [];
+
+    getAll(): Movie[] {
+        return this.movies;
+    }
+
+    getOne(id: number): Movie {
+        const movie = this.movies.find((movie) => movie.id === id);
+        if (!movie) {
+            throw new NotFoundException(`Can't not find movie with id ${id}`);
+        }
+
+        return movie;
+    }
+
+    deleteOne(id: number) {
+        this.getOne(id);
+        this.movies = this.movies.filter((movie) => movie.id !== id);
+    }
+
+    create(data: CreateMovieDto) {
+        this.movies.push({
+            id: this.movies.length + 1,
+            ...data,
+        });
+
+        return {
+            success: true,
+            id: this.movies.length,
+        };
+    }
+
+    update(id: number, updateData: UpdateMovieDto) {
+        const movie = this.getOne(id);
+
+        this.deleteOne(id);
+        this.movies.push({ ...movie, ...this.update });
+    }
+}
